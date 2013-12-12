d5eb1afda6a955b7d4f4d5a6b1fce4b166716785
knagelberg <knagelberg@salesforce.com>
2013-12-11 23:27:56 -0800

    cleaning up test

--- src/test/groovy/org/hearth4j/HearthEngineTest.groovy
+++ src/test/groovy/org/hearth4j/HearthEngineTest.groovy
@@ -1,15 +1,11 @@
 package org.hearth4j
 
-import com.google.inject.Guice
-import com.google.inject.Injector
-import org.hearth4j.guice.HearthModule
-
 import javax.inject.Inject
 
 class HearthEngineTest extends LibraryBasedTest {
 
     @Inject
-    DeckFactory deckFactory
+    HearthGame hearthGame
 
     def 'players have the correct # of cards after the mulligan stage'() {
         when:
@@ -17,18 +13,15 @@ class HearthEngineTest extends LibraryBasedTest {
         def player1 = new Player(deck: simpleDeck)
         def player2 = new Player(deck: simpleDeck)
 
-        Injector injector = Guice.createInjector(new HearthModule())
-        def game = injector.getInstance(HearthGame)
-
-        game.currentPlayer = player1
-        game.otherPlayer = player2
-        game.begin()
+        hearthGame.currentPlayer = player1
+        hearthGame.otherPlayer = player2
+        hearthGame.begin()
 
         then:
 
-        game.currentPlayer.hand.size() == 3
-        game.otherPlayer.hand.size() == 4
-        game.otherPlayer.hand.find { it.name == 'The Coin' }
+        hearthGame.currentPlayer.hand.size() == 3
+        hearthGame.otherPlayer.hand.size() == 4
+        hearthGame.otherPlayer.hand.find { it.name == 'The Coin' }
     }
 
     Deck getSimpleDeck() {
